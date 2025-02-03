a5892c4a5b4d45443140d4a8d588dc7700217c9e
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-02-03 21:32:23 +0200

    Creating Calculator app version 1

---
+++ Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Hello, Welcome to the calculator program");
+            
+            Console.Write("Please Enter your first Number: ");
+            Console.Write("Please Enter your first Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please Enter your second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
