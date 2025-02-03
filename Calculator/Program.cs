18e355b9118ab9e3e28de79f6269dcff18cfc6f7
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-02-03 21:56:32 +0200

    Finish v1 of clac app

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,15 +6,40 @@
         {
             int num1;
             int num2;
+            int operation;
+            int result;
 
             Console.WriteLine("Hello, Welcome to the calculator program");
             
             Console.Write("Please Enter your first Number: ");
-            Console.Write("Please Enter your first Number: ");
             num1 = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Please Enter your second Number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine("Enter Number of Operation you want to do:\n1. +\n2. -\n3. *\n4. /");
+            operation = Convert.ToInt32(Console.ReadLine());
+
+            if (operation == 1)
+            {
+                result = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {result}");
+            }
+            else if (operation == 2)
+            {
+                result = num1 - num2;
+                Console.WriteLine($"{num1} - {num2} = {result}");
+            }
+            else if (operation == 3)
+            {
+                result = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = {result}");
+            }
+            else
+            {
+                result = num1 / num2;
+                Console.WriteLine($"{num1} / {num2} = {result}");
+            }
 
 
         }
--- Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5892c4a5b4d45443140d4a8d588dc7700217c9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
